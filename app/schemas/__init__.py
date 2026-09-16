c64634763e57922ca86d150e708f0ff959ee164b
0ntee <107307259+0ntee@users.noreply.github.com>
2026-09-16 17:29:52 +0300

    Delete app directory

--- app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .agent import Action, Observation
-from .trajectory import TrajectoryStep, AgentTrajectory
-
-__all__ = ["Action", "Observation", "TrajectoryStep", "AgentTrajectory"] # Экспорт наружу для удобных импортов
